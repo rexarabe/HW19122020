6a68ed9aed649d7924123ae70f0aee031d78361d
rexarabe <ab28041993@gmail.com>
2020-12-19 12:34:35 +0200

    I add the third task about the fibonacci serie

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -26,3 +26,27 @@ console.log(c);
 const massiv = [1,-2,3,-4,-5,0,10,7,-8,9,4,5,-10,0,0,];
 const resultmassiv = massiv.filter(mass =>mass.valueOf() > 0);
 console.log(resultmassiv);
+
+
+//task 3
+// Получить массив , элементы которого являются числами фибоначчи. Размер массива (количество элементов) задает пользователь.
+function  Fibonacci(n){
+    let n1= 1, n2 = 1, f, temp, c;
+
+    for (c = n-2; c > 0, c--;){
+        temp = n1;
+        n1 = n1+n2;
+        n2 = temp;
+    }
+    f = n1;
+
+    return f;
+}
+
+let fibo = prompt(`Please write the number of element that you want to get in the fibonacci serie.`);
+
+arrfibo = [0,1,1,2];
+for (i=2; i <= fibo; i++){
+    arrfibo.push(Fibonacci(i));
+}
+console.log(arrfibo)
