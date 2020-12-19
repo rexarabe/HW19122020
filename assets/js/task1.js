1dd7bbee99235d6e93940b4db9a6a64f2cb99c32
rexarabe <ab28041993@gmail.com>
2020-12-19 11:38:26 +0200

    Initial commit

---
+++ assets/js/task1.js
@@ -0,0 +1,20 @@
+const wordsinfrench = [
+    'mère',
+    'père',
+    'frère',
+    'forêt',
+    'ordinateur'
+];
+
+const result = wordsinfrench.filter( (wordsinfrench) => wordsinfrench.length > 4);
+console.log(result);
+
+//2 var
+
+//task 1
+//Даны два массива. Объедините их и преобразовав в строку.
+const a= [2,5,3,4,6];
+const b = [ 5,6,7,9];
+const c = a.concat(b);
+
+console.log(c);
